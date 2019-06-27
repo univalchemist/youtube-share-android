336b245f09452faae68ec33c89afbc7a3d514640
ligwang <xiaoxing440824@outlook.com>
2019-06-27 10:11:21 +0800

    check store version

--- app/src/main/java/com/cynny/videoface/ui/main/MainActivity.kt
+++ app/src/main/java/com/cynny/videoface/ui/main/MainActivity.kt
@@ -2,24 +2,25 @@ package com.cynny.videoface.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ShareCompat
-import androidx.fragment.app.Fragment
 import com.cynny.videoface.R
 import com.cynny.videoface.ui.misc.ShareListener
 import com.cynny.videoface.ui.misc.service.ShareService
 import com.cynny.videoface.ui.misc.utils.ViewModelFactory
-import dagger.android.AndroidInjection
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.DaggerAppCompatActivity
-import dagger.android.support.HasSupportFragmentInjector
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
-import java.util.concurrent.TimeUnit
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import javax.inject.Inject
 import javax.inject.Provider
+import org.jsoup.Jsoup
+import java.io.IOException
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.content.pm.PackageInfo
+import android.net.Uri
+import androidx.appcompat.app.AlertDialog
+
 
 class MainActivity : DaggerAppCompatActivity(), ShareListener {
     @Inject
@@ -39,9 +40,93 @@ class MainActivity : DaggerAppCompatActivity(), ShareListener {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.main_activity)
-
+        checkUpdateVersion()
         viewModel.shareMessageLiveEvent.observe(this, this)
         onNewIntent(intent)
+    }
+
+    private fun checkUpdateVersion() {
+
+        val future = doAsync {
+            val storeVersion = getPlayStoreVersion()
+            val currentVersion = getCurrentVersion()
+            Log.e("ligwang", "storeVersion:::" + storeVersion)
+            Log.e("ligwang", "currentVersion:::" + currentVersion)
+            uiThread {
+                // use result here if you want to update ui
+                if (!currentVersion.equals(storeVersion)) {
+                    Log.e("ligwang", "new version is available to update on store")
+                    showDialog()
+                }
+            }
+        }
+    }
+
+    private fun getPlayStoreVersion(): String? {
+        var newVersion: String? = null
+
+        try {
+            val document = Jsoup.connect("https://play.google.com/store/apps/details?id=" + "com.cynny.videoface" + "&hl=en")
+                    .timeout(30000)
+                    .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+                    .referrer("http://www.google.com")
+                    .get()
+            if (document != null) {
+                val element = document.getElementsContainingOwnText("Current Version")
+                for (ele in element) {
+                    if (ele.siblingElements() != null) {
+                        val sibElemets = ele.siblingElements()
+                        for (sibElemet in sibElemets) {
+                            newVersion = sibElemet.text()
+                        }
+                    }
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return newVersion
+    }
+
+    private fun getCurrentVersion(): String? {
+        var version: String? = null
+        val manager = packageManager
+        var info: PackageInfo? = null
+        try {
+            info = manager.getPackageInfo(packageName, 0)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+
+        assert(info != null)
+        version = info!!.versionName
+        return version
+    }
+
+    private fun showDialog() {
+        val alertDialog: AlertDialog? = this.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage("New version is available on the Store.\nDo you want to install now?")
+                    .setPositiveButton("OK",
+                            DialogInterface.OnClickListener { dialog, id ->
+                                openStore()
+                            })
+            // Create the AlertDialog object and return it
+            builder.create()
+        }
+        alertDialog?.show()
+    }
+    private fun openStore() {
+        val appPackageName = packageName
+        try {
+            val appStoreIntent = Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName"))
+            appStoreIntent.setPackage("com.cynny.videoface")
+            startActivity(appStoreIntent)
+        } catch (anfe: android.content.ActivityNotFoundException) {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.cynny.videoface")))
+        }
+
     }
 
     override fun onNewIntent(intent: Intent?) {
